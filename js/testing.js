3462ab0f32ab350d4abb65a81bbe5503d10c4686
tomrogand <37114961+tomrogand@users.noreply.github.com>
2018-03-20 23:29:51 +0100

    Add files via upload

---
+++ js/testing.js
@@ -0,0 +1,19 @@
+
+(function(){
+  "use strict";
+  function greetMe(yourName) {
+      alert('Hello ' + yourName);
+}
+greetMe('World');
+})();
+
+
+var word1 = "Hello ";
+var word2 = "World";
+var bothwords = word1 + word2;
+document.write(bothwords);
+
+var firstnum = 2;
+var secondnum = 4;
+var total = firstnum + secondnum;
+document.write(total);
